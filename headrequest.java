df26b80bfd9d3f2542ab9d3f823fc6a30aa26366
dahiya18 <harmeetadahiya@gmail.com>
2020-06-12 00:40:11 -0700

    basic httpClient use

---
+++ headrequest.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class headrequest {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder().uri(URI.create("http://webcode.me"))
+                .method("HEAD", HttpRequest.BodyPublishers.noBody()) //since we do not have any body in the request
+                .build();
+
+        HttpResponse<Void> response = client.send(request,
+                HttpResponse.BodyHandlers.discarding()); //we send a request and recieve a body
+
+        HttpHeaders headers = response.headers();
+
+        headers.map().forEach((key, values) -> {
+            System.out.printf("%s: %s%n", key, values);
+        });
+    }
+}
