8824a19f9aea18ed8a174498ba6b14f5c8dce2c8
Nikita <nikvel1995@gmail.com>
2020-06-20 17:29:02 +0100

    Add AppContextAware:
    - add ApplicationContextAware interface
    - Application no longer has a main method, AppEntry has the main
    - AppEntry calls Application using $ gradle bootRun
    - add fatJar task to build.gradle
    - change query function to GLOBAL_QUOTE

--- stockexchange-source/src/main/java/is/equinox/cloudflow/source/stockexchange/Application.java
+++ stockexchange-source/src/main/java/is/equinox/cloudflow/source/stockexchange/Application.java
@@ -11,17 +11,12 @@ import java.io.IOException;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"is.equinox"})
-//@EnableBinding(Source.class)
 public class Application {
 
-//    public static void main(String[] args){
-//        SpringApplication.run(Application.class, args);
-//    }
-
-    public static void main(String[] args) throws IOException {
-        final String function = "TIME_SERIES_INTRADAY";
+    public static void queryStocks() throws IOException {
+        final String function = "GLOBAL_QUOTE";
         final String symbol = "TSLA";
-        final String interval = "1min";
+        final String interval = null;
         final String api = "A5K060RWV2X3BVP5";
 
         HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
