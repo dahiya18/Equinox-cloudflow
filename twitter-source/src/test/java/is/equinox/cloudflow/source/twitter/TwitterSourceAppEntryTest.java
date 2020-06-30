0b1dab07768835b740d609d4a926d9c3a9772e2c
dahiya18 <harmeetadahiya@gmail.com>
2020-06-29 17:28:33 -0700

    added a authorisation test for twitterApp

--- twitter-source/src/test/java/is/equinox/cloudflow/source/twitter/TwitterSourceAppEntryTest.java
+++ twitter-source/src/test/java/is/equinox/cloudflow/source/twitter/TwitterSourceAppEntryTest.java
@@ -1,12 +1,26 @@
-package is.equinox.cloudflow.source.stockexchange;
+package is.equinox.cloudflow.source.twitter;
 
 import org.junit.jupiter.api.Test;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TwitterSourceAppEntryTest {
 
-	@Test
-	public void sample_test() {
-          //assertEquals("apple", "apple");
-	}
+    /**
+     * Exception test for wrong authorisation keys
+     */
+    @Test
+    public void auth_test(){
+        twitterClass tc = new twitterClass();
+        tc.setAuth("abc", "abc", "abc", "abc");
+        Twitter tw = tc.makeConnection();
+	    assertThrows(TwitterException.class, () -> tc.searchQuery(tw,"tesla"));
+    }
+
+
+
+
 
 }
