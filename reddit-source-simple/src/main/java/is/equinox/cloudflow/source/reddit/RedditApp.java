210d9be9b016ed930381472641517923b01b0ca9
Gagan Bhatia <gagan3012@users.noreply.github.com>
2020-07-28 15:32:10 +0530

    SCDF files added in reddit-source-simple folder

---
+++ reddit-source-simple/src/main/java/is/equinox/cloudflow/source/reddit/RedditApp.java
@@ -0,0 +1,66 @@
+package is.equinox.cloudflow.source.reddit;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.NetworkAdapter;
+import net.dean.jraw.http.NetworkException;
+import net.dean.jraw.http.OkHttpNetworkAdapter;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.Submission;
+import net.dean.jraw.models.SubredditSort;
+import net.dean.jraw.oauth.Credentials;
+import net.dean.jraw.oauth.OAuthHelper;
+import net.dean.jraw.pagination.DefaultPaginator;
+import net.dean.jraw.references.SubredditReference;
+
+
+public class RedditApp {
+    static String userName = "gaganeq";
+    static String passWord = "@1234@abcd@";
+    static String apiKey = "lnrJRkv6cq-uzA";
+    static String secret = "IY8Za1Cqj89kP7t18Yd-UNlBA-Q";
+
+    public static UserAgent userAgent = new UserAgent("desktop","name.Java-API","v0.1",userName);
+    public static Credentials credentials = Credentials.script(userName,passWord, apiKey, secret);
+    public static NetworkAdapter networkAdapter = new OkHttpNetworkAdapter(userAgent);
+    public static RedditClient redditClient = OAuthHelper.automatic(networkAdapter, credentials);
+
+    public static String queryReddit() {
+        String subR = "tesla";
+        int n = 5;
+        StringBuilder Posts = new StringBuilder();
+        SubredditReference subreddit = redditClient.subreddit(subR);
+
+        //check if subreddit is valid before moving on
+        try{
+            subreddit.about().getFullName();
+        }catch (Exception E){
+            return "Error Subreddit not found";
+        }
+
+        //Get only n posts from the category the user input
+        DefaultPaginator<Submission> paginator = subreddit.posts().limit(n).sorting(SubredditSort.NEW).build();
+
+        Listing<Submission> firstPage = null;
+
+        //if the subreddit doesn't exist then return error
+        try {
+            firstPage = paginator.next();
+        }catch (NetworkException nE) {
+            return "Error";
+        }
+
+        //iterate through page to get posts
+        for (Submission post : firstPage) {
+            Posts.append( /*"Title:     "+*/ post.getTitle() + "\n" +
+                            post.getSelfText() + "\n" );
+
+
+        }
+        return Posts.toString();
+    }
+
+}
+
+
+
---
+++ reddit-source-simple/src/main/java/is/equinox/cloudflow/source/reddit/RedditInterface.java
@@ -0,0 +1,29 @@
+package is.equinox.cloudflow.source.reddit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@EnableBinding(Processor.class)
+public class RedditInterface {
+    Logger logger = LoggerFactory.getLogger(RedditInterface.class);
+
+    @Bean
+    @InboundChannelAdapter(value = Source.OUTPUT,poller = @Poller(fixedDelay = "10000", maxMessagesPerPoll = "1"))    public MessageSource<String> RedditSentimentAnalysis() throws IOException {
+        String reddit = RedditApp.queryReddit();
+        logger.info("\n\n {}" , reddit);
+        return ()-> MessageBuilder.withPayload(reddit).build();
+    }
+
+}
