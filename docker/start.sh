6e69ab37820a7b3976c545f0806fbab6ede49b91
Gagan Bhatia <gagan3012@users.noreply.github.com>
2020-08-20 22:31:45 +0530

    Merge Ready

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # put your folder path here
-
 # export HOST_MOUNT_PATH=/Users/pixel/Development/shadow/cloudflow/stockexchange-source/build/libs
-
 export DATAFLOW_VERSION=2.5.1.RELEASE
-
 export SKIPPER_VERSION=2.4.1.RELEASE
 docker-compose up
